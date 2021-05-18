31ab35e1a45beebf1f479a73bdc01f12c49b16fe
odd-star <k.kurosawa70134@gmail.com>
2021-05-18 12:30:48 +0900

    カートに購入ボタン追加

--- cart_show.php
+++ cart_show.php
@@ -1,68 +1,77 @@
-<?php require '_login_check.php'?>
+<?php require '_login_check.php' ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <title>買い物かご画面</title>
-    <link rel="stylesheet" href="navi.css">
+	<meta charset="UTF-8">
+	<title>買い物かご画面</title>
+	<link rel="stylesheet" href="navi.css">
 
 </head>
 <br>
+
 <body>
-    <sign>
-    <?php require '_nav.php'; ?>
-<?php
-if (!empty($_SESSION['products'])) {
-?>
-    <table>
-		<th>　　　　</th>
-		<th>商品名</th>
-		<th>価格</th>
-		<th>個数</th>
-		<th>小計</th>
+	<sign>
+		<?php require '_nav.php'; ?>
 		<?php
-		$total = 0;
-		foreach ($_SESSION['products'] as $code => $product) {
+		if (!empty($_SESSION['products'])) {
 		?>
-			<tr>
-				<td><?= $code ?></td>
-				<td><a href="product_show.php?name=<?=$product['name']?>"><?= $product['name'] ?></a></td>
-				<td><?= $product['cost'] ?></td>
-				<td><?= $product['count'] ?></td>
+			<table>
+				<th>　　　　</th>
+				<th>商品名</th>
+				<th>価格</th>
+				<th>個数</th>
+				<th>小計</th>
 				<?php
-				$subtotal = $product['cost'] * $product['count'];
-				$total += $subtotal;
+				$total = 0;
+				foreach ($_SESSION['products'] as $code => $product) {
 				?>
-				<td><?= $subtotal ?></td>
-				<td><a href="cart_destroy.php?code=<?= $code ?>">削除</a></td>
-			</tr>
+					<tr>
+						<td><?= $code ?></td>
+						<td><a href="product_show.php?name=<?= $product['name'] ?>"><?= $product['name'] ?></a></td>
+						<td><?= $product['cost'] ?></td>
+						<td><?= $product['count'] ?></td>
+						<?php
+						$subtotal = $product['cost'] * $product['count'];
+						$total += $subtotal;
+						?>
+						<td><?= $subtotal ?></td>
+						<td><a href="cart_destroy.php?code=<?= $code ?>">削除</a></td>
+					</tr>
+				<?php
+				}
+				?>
+				<tr>
+					<td>合計</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td><?= $total ?></td>
+					<td></td>
+
+				</tr>
+				<tr>
+					<td>
+						<a href="order_new.php"><input type="submit" value="購入"></a>
+					</td>
+				</tr>
+			</table>
 		<?php
+		} else {
+		?>
+			カートに商品がありません。
+			<?php
+		}
+		if (isset($_SESSION['orderError'])) {
+			foreach ($_SESSION['orderError'] as $key => $item) {
+			?>
+				<p><?= $item ?></p>
+		<?php
+			}
 		}
 		?>
-		<tr>
-			<td>合計</td>
-			<td></td>
-			<td></td>
-			<td></td>
-			<td><?= $total ?></td>
-			<td></td>
-		</tr>
-    </table>
-<?php
-} else {
-?>
-    カートに商品がありません。
-<?php
-}
-if(isset($_SESSION['orderError'])) {
-foreach($_SESSION['orderError']as $key => $item){
-?>
-<p><?= $item ?></p>
-<?php
-}}
-?>
-    </sign>
+	</sign>
 </body>
+
 
 </html>
